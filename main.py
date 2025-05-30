07d0b2b22db32a439baed36bf0cb8bd0f6e7a449
RETR0-OS <aadityajindal12@gmail.com>
2025-05-29 22:59:57 -0700

    add args parser

--- main.py
+++ main.py
@@ -1,4 +1,30 @@
-import torch
-from torchvision.models.swin_transformer import swin_v2_b
-from torchvision.models import Swin_V2_B_Weights
+import os
+import argparse
 
+
+def arg_parser():
+    parser = argparse.ArgumentParser(description="CheXpert Practice")
+    parser.add_argument("--gpu", type=int, default=0, help="GPU index to use")
+    parser.add_argument("--img_size", type=int, default=224, help="Input image resolution")
+    parser.add_argument("--batch_size", type=int, default=32, help="Batch size for training")
+    parser.add_argument("--epochs", type=int, default=10, help="Number of training epochs")
+    parser.add_argument("--pretrained_weights", type=str, default=None, help="ImageNet_1k | Random | Path to weights")
+    parser.add_argument("--output_dir", type=str, default="./output", help="Directory to save outputs")
+    parser.add_argument("--lr", type=float, default=1e-3, help="Learning rate for the optimizer")
+    parser.add_argument("--optim" , type=str, default="adamw", choices=["adamw", "adam", "sgd"], help="Optimizer to use")
+    parser.add_argument("--dataset_path", type=str, required=True, help="Path to the dataset directory")
+    return parser.parse_args()
+
+
+def main(args: argparse.Namespace):
+    print("*"*50)
+    print(args)
+    print()
+    print("*"*50, end="\n\n")
+
+    logs_dir = os.path.join(os.path.abspath(args.output_dir),"logs")
+    models_dir = os.path.join(os.path.abspath(args.output_dir),"models")
+    os.makedirs(logs_dir, exist_ok=True)
+    os.makedirs(models_dir, exist_ok=True)
+    print(f"Logs will be saved to: {logs_dir}")
+    print(f"Models will be saved to: {models_dir}", end="\n\n")
